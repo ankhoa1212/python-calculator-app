1b3fbc14ce39d94a41c315711ac1bb9911906630
Austin <ankhoa1212@gmail.com>
2024-03-17 15:52:38 -0700

    add display and buttons

--- calculator.py
+++ calculator.py
@@ -1,10 +1,21 @@
 # to handle application's termination and exit status
 import sys
 
-# import QApplication and all the required widgets
-from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget
+# import PyQt6 components
+from PyQt6.QtCore import Qt
+from PyQt6.QtWidgets import (
+    QApplication,
+    QGridLayout,
+    QLineEdit,
+    QMainWindow,
+    QPushButton,
+    QVBoxLayout,
+    QWidget,
+)
 
 WINDOW_SIZE = 250
+DISPLAY_HEIGHT = 50
+BUTTON_SIZE = 40
 
 # if command-line arguments are needed, then can use sys.argv to handle them instead
 args = []
@@ -15,8 +26,35 @@ class CalculatorWindow(QMainWindow):
 		super().__init__()
 		self.setWindowTitle("Calculator")
 		self.setFixedSize(WINDOW_SIZE, WINDOW_SIZE)
+		self.generalLayout = QVBoxLayout()
 		centralWidget = QWidget(self)
+		centralWidget.setLayout(self.generalLayout)
 		self.setCentralWidget(centralWidget)
+		self._createDisplay()
+		self._createButtons()
+	def _createDisplay(self):
+		self.display = QLineEdit()
+		self.display.setFixedHeight(DISPLAY_HEIGHT)
+		self.display.setAlignment(Qt.AlignmentFlag.AlignRight)
+		self.display.setReadOnly(True)
+		self.generalLayout.addWidget(self.display)
+	def _createButtons(self):
+		self.buttonMap = {}
+		buttonsLayout = QGridLayout()
+		keyBoard = [
+			["7", "8", "9", "/", "C"], 
+			["4", "5", "6", "*", "("], 
+			["1", "2", "3", "-", ")"], 
+			["0", "00", ".", "+", "="]
+		]
+		for row, keys in enumerate(keyBoard):
+            		for col, key in enumerate(keys):
+                		self.buttonMap[key] = QPushButton(key)
+                		self.buttonMap[key].setFixedSize(BUTTON_SIZE, BUTTON_SIZE)
+                		buttonsLayout.addWidget(self.buttonMap[key], row, col)
+
+		self.generalLayout.addLayout(buttonsLayout)
+
 
 def main():
 	"""Calculator's main function"""
